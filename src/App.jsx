a4b69d41c43359a68cdf68eb4e2b41523ffb50ad
4vm <amadeumrzr@gmail.com>
2022-02-19 10:36:21 -0300

    projeto criado

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import AddGoal from './components/AddGoal'
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Gffffoals</h1>
+      <AddGoal/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/AddGoal.jsx
@@ -0,0 +1,11 @@
+const AddGoal = () => {
+  return (
+    <form>
+      <label htmlFor="">Digite uma tarefa: </label>
+      <input type="text" />
+      <button>Adicionar</button>
+    </form>
+  )
+}
+
+export default AddGoal
